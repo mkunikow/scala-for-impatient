924dd3fe7ac222bec4b893ecedc03152e8cc3e14
Michal Kunikowski <michal.kunikowski@gtech.com>
2014-11-26 16:54:18 +0100

    chapter 11 e1

---
+++ src/main/scala/exercises/chapter11/e8.scala
@@ -0,0 +1,60 @@
+package exercises.chapter11
+
+/**
+ * Created by mkunikowski on 11/26/2014.
+ */
+object MainE8 extends App {
+
+  type Row = Vector[Double]
+  type Matrix = Vector[Row]
+
+  class RichMatrix private (private val matrix: Matrix) {
+
+    val rowCount = matrix.length
+    val colCount = matrix.head.length
+
+    def dotProd(v1:Row,v2:Row) =
+      v1.zip( v2 ).map{ t:(Double,Double) => t._1 * t._2 }.reduceLeft(_ + _)
+
+    def sumRows(v1:Row, v2:Row) =
+      v1 zip v2 map {t:(Double,Double) => t._1  +  t._2 }
+
+    def transpose(m: Matrix):Matrix =
+      if(m.head.isEmpty) Vector() else m.map(_.head) +: transpose(m.map(_.tail))
+
+    def mXm( m1:Matrix, m2:Matrix) =
+      for( m1row <- m1 ) yield
+        for( m2col <- transpose(m2) ) yield
+          dotProd( m1row, m2col )
+
+    def sum(m1: Matrix, m2: Matrix): Matrix = {
+      require(m1.length == m2.length)
+      require(m1.head.length == m2.head.length)
+
+      m1 zip m2 map (t => sumRows(t._1, t._2))
+    }
+
+    def apply(i:Int, j:Int) = matrix(i)(j)
+
+    def *(that:RichMatrix) = RichMatrix(mXm( this.matrix, that.matrix))
+
+    def +(that: RichMatrix) = RichMatrix(sum(this.matrix, that.matrix))
+
+    def toStr = matrix.map{
+      _.map{"\t" + _}.reduceLeft(_ + _) + "\n"
+    }.reduceLeft(_ + _)
+
+    override def toString  = toStr
+
+
+  }
+
+  object RichMatrix {
+    def apply(matrix:Matrix) = new RichMatrix(matrix)
+  }
+
+  println(RichMatrix(Vector(Vector(1,1,1), Vector(2,2,2))))
+  println(RichMatrix(Vector(Vector(1,1), Vector(2,2))) * RichMatrix(Vector(Vector(1,1), Vector(2,2))) )
+  println(RichMatrix(Vector(Vector(1,1), Vector(1,1))) + RichMatrix(Vector(Vector(1,1), Vector(1,1))) )
+
+}
